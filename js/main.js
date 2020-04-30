eef348d4642c2838a7fbd3eb7ec9ef33fbdafcbe
Felipe Nunes <felipeln.99@gmail.com>
2020-04-30 17:00:47 -0300

    new update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 const gameSource = {
     board: ['','','','','','','','',''],
+
+    player: {
+        options: ['X', 'O'],
+        options_index: 0,
+        changePlayerOption() {
+            this.options[this.options_index] === 0 ? 1 : 0;
+        }
+    },
+
+    drawField(containerID) {
+        let content = '';
+
+        for(position in this.board) {
+            content += `<div>${this.board[position]}</div>`;
+        }
+
+        containerID.innerHTML = content;
+    }
 }
